6182b7482bed3b3bf9e41b45dfa46b597eb16655
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-11-14 11:49:11 +0900

    SPAとルーティングについて#7

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
-  <div id="app">
+  <div style="width: 700px; margin: auto; padding-top: 50px;">
+    <nav>
+      <router-link
+        to="/"
+        active-class="link--active"
+        class="link"
+        exact
+      >Home</router-link>
+      <router-link
+        :to="{ name: 'users-profile', params: { id: 1 } }"
+        active-class="link--active"
+        class="link"
+        exact
+      >Users</router-link>
+    </nav>
+    <Home></Home>
     <router-view></router-view>
   </div>
 </template>
+
+
+<style>
+.link {
+  margin-right: 10px;
+}
+.link--active {
+  font-size: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
     <h2>Home</h2>
+    <button @click="toUsers">Usersのページにいく</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    toUsers() {
+      this.$router.push({
+        name: 'users-id-profile',
+        params: { id: 1 }
+      });
+    }
+  }
+}
+</script>

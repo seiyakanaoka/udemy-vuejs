832dc245b3c991a8145e06006d7b5f8974d2c50a
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-11-14 16:39:49 +0900

    finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
     <h3>Home</h3>
-    <h3>Hello</h3>
     <button @click="toUsers">Usersのページにいく</button>
+    <p>{{ doubleCount }}</p>
+    <p>{{ tripleCount }}</p>
+    <input
+      type="text"
+      v-model="message"
+    >
+    <p>{{ message }}</p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
+  computed: {
+    ...mapGetters("count", ["doubleCount", "tripleCount"]),
+    // doubleCount() {
+    //   return this.$store.getters["count/doubleCount"];
+    // },
+    // tripleCount() {
+    //   return this.$store.getters["count/tripleCount"];
+    // },
+    message: {
+      get() {
+        return this.$store.getters.message;
+      },
+      set(value) {
+        this.$store.dispatch("updateMessage", value)
+      }
+    }
+  },
   methods: {
     toUsers() {
       this.$router.push({
